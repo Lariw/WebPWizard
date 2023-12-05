2431e1dcfb7b5a20d73ec1e5d0d09a59fd9ac7d3
Kamil Hajduła <98982966+Lariw@users.noreply.github.com>
2023-12-05 23:37:18 +0100

    Added some styles in main section.

--- GUI/script/convertImage.js
+++ GUI/script/convertImage.js
@@ -1,43 +1,39 @@
-const sharp = require('sharp');
-const startConvertBtn = document.querySelector('.js-startConvert');
+const sharp = require("sharp");
+const startConvertBtn = document.querySelector(".js-startConvert");
 
-startConvertBtn.addEventListener('click', () => {
-    convertToWebP();
-})
-
+startConvertBtn.addEventListener("click", () => {
+  convertToWebP();
+});
 
 async function convertToWebP() {
-    const fileInput = document.querySelector('.js-file');
-    const file = fileInput.files[0];
+  const fileInput = document.querySelector(".js-file");
+  const file = fileInput.files[0];
 
-    if (!file) {
-      alert('Please select a PNG file.');
-      return;
+  if (!file) {
+    alert("Please select a PNG file.");
+    return;
+  }
+
+  const reader = new FileReader();
+
+  reader.onload = async function (event) {
+    const inputBuffer = new Uint8Array(event.target.result);
+
+    try {
+      const webpBuffer = await sharp(inputBuffer).webp().toBuffer();
+      downloadFile(webpBuffer, "converted.webp");
+    } catch (error) {
+      console.error("Conversion error:", error);
     }
+  };
 
-    const reader = new FileReader();
+  reader.readAsArrayBuffer(file);
+}
 
-    reader.onload = async function (event) {
-      const inputBuffer = new Uint8Array(event.target.result);
-      
-
-      try {
-        const webpBuffer = await sharp(inputBuffer).webp().toBuffer();
-        downloadFile(webpBuffer, 'converted.webp');
-      } catch (error) {
-        console.error('Conversion error:', error);
-      }
-    };
-
-    reader.readAsArrayBuffer(file);
-  }
-
-
-  function downloadFile(buffer, fileName) {
-    const blob = new Blob([buffer]);
-    const link = document.createElement('a');
-    link.href = window.URL.createObjectURL(blob);
-    link.download = fileName;
-    link.click();
-  }
-
+function downloadFile(buffer, fileName) {
+  const blob = new Blob([buffer]);
+  const link = document.createElement("a");
+  link.href = window.URL.createObjectURL(blob);
+  link.download = fileName;
+  link.click();
+}
