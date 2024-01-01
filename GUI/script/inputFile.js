5be590d4a870419b9762e45f67dfc6583fb580c6
Kamil Hajduła <98982966+Lariw@users.noreply.github.com>
2024-01-01 20:41:52 +0100

    Added deletion of previously added photos.

--- GUI/script/inputFile.js
+++ GUI/script/inputFile.js
@@ -1,6 +1,7 @@
 const fileInput = document.querySelector(".js-file");
 const inputContainer = document.querySelector(".js-inputContainer");
 const nameImgContainer = document.querySelector(".js-fileNameContainer");
+const imagesInterface = document.querySelector(".imagesInterface");
 const handleFileSelect = () => {
   nameImgContainer.innerHTML = "";
 
@@ -15,12 +16,13 @@ const handleFileSelect = () => {
 
     const fileNameDiv = document.createElement("div");
     fileNameDiv.textContent = fileName;
-    fileNameDiv.classList.add('titleImage')
+    fileNameDiv.classList.add("titleImage");
 
     const fileNameDeleteIcon = document.createElement("div");
-    fileNameDeleteIcon.classList.add('deleteIcon')
-    fileNameDeleteIcon.innerHTML = '<i class="deletePhotoBtn fa-solid fa-trash fa-xl"></i>'
-    
+    fileNameDeleteIcon.classList.add("deleteIcon");
+    fileNameDeleteIcon.innerHTML =
+      '<i class="deletePhotoBtn fa-solid fa-trash fa-xl"></i>';
+
     fileItemDiv.appendChild(fileNameDeleteIcon);
 
     fileItemDiv.appendChild(fileNameDiv);
@@ -30,7 +32,7 @@ const handleFileSelect = () => {
       thumbnailDiv.classList.add("thumbnail");
 
       const thumbnailImg = document.createElement("img");
-      thumbnailImg.classList.add('thumbnailImg')
+      thumbnailImg.classList.add("thumbnailImg");
       thumbnailImg.src = URL.createObjectURL(file);
       thumbnailImg.alt = fileName;
 
@@ -40,10 +42,51 @@ const handleFileSelect = () => {
 
     nameImgContainer.appendChild(fileItemDiv);
   }
+
+  deletePhoto();
 };
 
 fileInput.addEventListener("change", () => {
+  imagesInterface.style.display = "block";
   handleFileSelect();
+  showImagesInterface();
+});
+
+const showImagesInterface = () => {
   nameImgContainer.style.display = "block";
   inputContainer.style.display = "none";
-});
+  imagesInterface.style.display = "block";
+};
+
+const showMainInterface = () => {
+  nameImgContainer.style.display = "none";
+  inputContainer.style.display = "block";
+  imagesInterface.style.display = "none";
+  const fileForm = document.querySelector(".fileForm");
+  fileForm.reset();
+};
+
+const deletePhoto = () => {
+  let deletePhotoBtn = document.querySelectorAll(".deletePhotoBtn");
+
+  let iconsLenght = deletePhotoBtn.length;
+
+  if (iconsLenght == 1) {
+    deletePhotoBtn = document.querySelector(".deletePhotoBtn");
+    deletePhotoBtn.addEventListener("click", () => {
+      iconsLenght -= 1;
+      deletePhotoBtn.parentElement.parentElement.style.display = "none";
+      showMainInterface();
+    });
+  } else {
+    deletePhotoBtn.forEach((icon) => {
+      icon.addEventListener("click", () => {
+        icon.parentElement.parentElement.style.display = "none";
+        iconsLenght -= 1;
+        if (iconsLenght == 0) {
+          showMainInterface();
+        }
+      });
+    });
+  }
+};
